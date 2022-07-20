3fb2995cd6c24800b3c69b7312ba6b178cb57bc0
Эльвина Фаттахова <99656190+elvinaf1996@users.noreply.github.com>
2022-07-20 13:36:05 +0300

    Add files via upload

---
+++ src/test/java/tests/OrderWithIncorrectPhoneTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.junit.Test;
+import pages.BasketPage;
+import pages.HomePage;
+import pages.ProductPage;
+import pages.WearPage;
+
+public class OrderWithIncorrectPhoneTest extends BaseTest {
+
+    @Test()
+    public void checkingSortProductsByCost() {
+        HomePage homePage = new HomePage();
+        homePage.openPage()
+                .clickGoToWear();
+
+        WearPage wearPage = new WearPage();
+        wearPage.clickStoreProduct("ФУТБОЛКА ПОЛО ЧЕРНАЯ (М)");
+        ProductPage productPage = new ProductPage();
+        productPage.clickAddToBasketButton();
+        BasketPage basketPage = new BasketPage();
+        basketPage.sendName("Тестов Тест Тестович")
+                .sendPhone("0000000000")
+                .sendRegion("Тест")
+                .sendAddress("Тест Тестовая 1")
+                .sendReceiverName("Тестов Тест Тестович")
+                .sendStreet("пр-кт Ленина")
+                .sendHouse("1")
+                .sendOffice("1")
+                .clickOrderButton();
+        basketPage.checkPhoneErrorText("Укажите, пожалуйста, корректный номер телефона");
+        basketPage.checkMainErrorText("Укажите, пожалуйста, корректный номер телефона");
+
+
+
+    }
+}
